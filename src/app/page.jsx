e37bcb7f74a0079d1326912bd79b551340e81f63
Isabella <isabella.b.rosa6@aluno.senai.br>
2025-09-09 11:45:16 -0300

    refactor: update colors

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Isabella from '../../public/img/isabella.jpg'
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-6 py-10">
-      <div className="w-full max-w-4xl bg-red-900 rounded-3xl shadow-lg flex flex-row items-center gap-0 p-15 shadow-gray-400">
+      <div className="w-full max-w-4xl bg-blue-900 rounded-3xl shadow-lg flex flex-row items-center gap-0 p-15 shadow-gray-400">
         <div className="flex-shrink-0 pr-8 flex items-center h-full">
           <Image
             src={Isabella}
@@ -15,7 +15,7 @@ export default function Home() {
           />
         </div>
         <div className="flex flex-col items-center gap-4 text-white">
-          <h1 className="font-bold text-xl">Bem-vindo ao Crystal Amour!</h1>
+          <h1 className="font-bold text-xl">Bem-vindo ao Cosmic Blend!</h1>
           <p>
             Meu nome é Isabella Borin de Moraes Rosa, sou estudante do SENAI de Valinhos e estou matriculada no curso Técnico de Desenvolvimento de Sistemas (2TDS1).
           </p>
