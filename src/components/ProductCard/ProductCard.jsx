26325bff4034de082cda4f2d3acf8aaea6aff1f8
Isabella Borin <158210136+isab5@users.noreply.github.com>
2025-09-09 15:48:40 -0300

    Replace img with Image component for thumbnails

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,10 @@
 import styles from "./ProductCard.module.css";
+import Image from "next/image"
 
 export default function ProductCard({product, onClick }) {
     return (
         <div className={styles.card} onClick={onClick}>
-            <img src={product.thumbnail} className={styles.avatar} alt={product.title} />
+            <Image src={product.thumbnail} className={styles.avatar} alt={product.title} width={200} height={200} />
             <h3 className={styles.title}>{product.title}</h3>
             <p>{product.description}</p>
             <p>{product.category}</p>
