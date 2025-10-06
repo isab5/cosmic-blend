9cc72ccca36b491d643d4ecddf46e86a87c4991f
Isabella <isabellaborin.tds1.senai@gmail.com>
2025-10-05 21:19:24 -0300

    refactor:change loading

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,13 +1,15 @@
+import styles from './loading.module.css';
+
 export default function Loading() {
     return (
-        <div className="loading-container">
-            <div className="spinner">
-                <div className="bounce1"></div>
-                <div className="bounce2"></div>
-                <div className="bounce3"></div>
+        <div className={styles.loadingContainer}>
+            <div className={styles.spinner}>
+                <div className={styles.bounce1}></div>
+                <div className={styles.bounce2}></div>
+                <div className={styles.bounce3}></div>
             </div>
-            <h3>Carregando conteúdo...</h3>
-            <p>Preparando tudo para você</p>
+            <h3 className={styles.loadingTitle}>Carregando conteúdo...</h3>
+            <p className={styles.loadingSubtitle}>Preparando tudo para você</p>
         </div>
     )
 }
